de1621ad52fedc49a847ea0e88b6ca150a939963
kian <kian.mohageri@paperg.com>
2013-06-02 23:30:46 -0700

    fix typo

--- manifests/mod/php.pp
+++ manifests/mod/php.pp
@@ -1,8 +1,8 @@
 class apache::mod::php {
   apache::mod { 'php5': }
-  file { 'php5.conf':
+  file { 'php.conf':
     ensure  => file,
     path    => "${apache::mod_dir}/php5.conf",
-    content => template('apache/mod/php5.conf.erb'),
+    content => template('apache/mod/php.conf.erb'),
   }
 }
