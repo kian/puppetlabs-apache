143f75675e8a3eef7aa6bfc0823cd18ec54e4d68
kian <kian.mohageri@paperg.com>
2013-06-02 23:26:14 -0700

    php->php5

--- manifests/mod/php.pp
+++ manifests/mod/php.pp
@@ -1,8 +1,8 @@
 class apache::mod::php {
   apache::mod { 'php5': }
-  file { 'php.conf':
+  file { 'php5.conf':
     ensure  => file,
-    path    => "${apache::mod_dir}/php.conf",
-    content => template('apache/mod/php.conf.erb'),
+    path    => "${apache::mod_dir}/php5.conf",
+    content => template('apache/mod/php5.conf.erb'),
   }
 }
